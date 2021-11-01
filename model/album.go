0fa74afea01ad005d413f83699236ed6be311230
yisfx <wangsaihaizai@outlook.com>
2021-11-01 20:59:31 +0800

    sort album

--- model/album.go
+++ model/album.go
@@ -1,5 +1,7 @@
 package model
 
+import "albumservice/framework/utils"
+
 ///相册
 type Album struct {
 	Name        string
@@ -30,4 +32,31 @@ type PictureUri struct {
 	Name      string `json:"n"`
 	Type      string `json:"t"`
 	Datetime  string `json:"d"`
+}
+
+type AlbumList []*Album
+
+func (I AlbumList) Len() int {
+	return len(I)
+}
+func (I AlbumList) Less(i, j int) bool {
+
+	dateI := utils.DateTime.Parse(I[i].Date)
+	dateJ := utils.DateTime.Parse(I[j].Date)
+	if dateI.Year != dateJ.Year {
+		return dateI.Year > dateJ.Year
+	}
+	if dateI.Month != dateJ.Month {
+		return dateI.Month > dateJ.Month
+	}
+	if dateI.Day != dateJ.Day {
+		return dateI.Day > dateJ.Day
+	}
+	if dateI.Hour != dateJ.Hour {
+		return dateI.Hour > dateJ.Hour
+	}
+	return false
+}
+func (I AlbumList) Swap(i, j int) {
+	I[i], I[j] = I[j], I[i]
 }
